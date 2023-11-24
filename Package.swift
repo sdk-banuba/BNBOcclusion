e02941c6d1d7a8fb5e58775547f65a71e1d649da
ci <ci@banuba.com>
2023-11-24 17:07:25 +0300

    1.9.1-86-g65c1c833d4

---
+++ Package.swift
@@ -0,0 +1,84 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.9.1-86-g65c1c833d4"
+
+let package = Package(
+    name: "BNBOcclusion",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBOcclusion",
+            targets: [
+                "BNBOcclusion",
+                "BNBOcclusion_BNBSdkCore",
+                "BNBOcclusion_BNBEffectPlayer",
+                "BNBOcclusion_BNBScripting",
+                "BNBOcclusion_BNBFaceTracker"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBEffectPlayer.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBScripting.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBFaceTracker.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBOcclusion",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-g65c1c833d4/BNBOcclusion.zip",
+            checksum: "46afa60a00e8f45f6349fe16004e59996c7fd49ff12260ccd1db39e9a853bc27"
+        ),
+        .target(
+            name: "BNBOcclusion_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBOcclusion_BNBEffectPlayer",
+            dependencies: [
+                .product(
+                    name: "BNBEffectPlayer",
+                    package: "BNBEffectPlayer"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBOcclusion_BNBScripting",
+            dependencies: [
+                .product(
+                    name: "BNBScripting",
+                    package: "BNBScripting"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBOcclusion_BNBFaceTracker",
+            dependencies: [
+                .product(
+                    name: "BNBFaceTracker",
+                    package: "BNBFaceTracker"
+                ),
+            ]
+        ),
+    ]
+)
