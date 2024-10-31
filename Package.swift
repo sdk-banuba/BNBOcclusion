0f4a9d47134fd825744ccfd58f19fcb9cbfcde5c
ci <ci@banuba.com>
2024-10-31 13:02:07 +0300

    1.14.1-170-g93323bc96

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-170-g91aac4bda"
+let bnbPackageVersion: Version = "1.14.1-170-g93323bc96"
 
 let package = Package(
     name: "BNBOcclusion",
@@ -41,8 +41,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBOcclusion",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-170-g91aac4bda/BNBOcclusion.zip",
-            checksum: "efebdea3a8a0413a47cd03977a37fd620e38a8a5178deea8821f7e3a66946778"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-170-g93323bc96/BNBOcclusion.zip",
+            checksum: "f162f5f0ad30f74b73ed413e97d8914240ad5b7773022220b29d568b2a5b44cd"
         ),
         .target(
             name: "BNBOcclusion_BNBSdkCore",
